ca02847d7a3840757784160b901086a0621ba068
John Theodore <johntheodore@github.com>
2019-05-28 15:19:47 -0700

    make the secrets.py file compatible with the travis build system

--- data_sources.py
+++ data_sources.py
@@ -2,10 +2,10 @@
 
 # ./data/geocode
 GEOCODE_CACHED_JSON_FILENAME = './data/geocode/geo_data.json'
-GEOCODE_FINAL_CSV_FILENAME = 'data/geocode/cities_geo_lat_long_address.csv'
+GEOCODE_FINAL_CSV_FILENAME = './data/geocode/cities_geo_lat_long_address.csv'
 
 # ./data/census
-CENSUS_POPULATION_2017_CSV_FILENAME = 'data/census/PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
+CENSUS_POPULATION_2017_CSV_FILENAME = './data/census/PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
 
 # ./data/experian
 EXPERIAN_SOURCE_CSV_DIR = './data/experian/csv_files/'
---
+++ secrets.py
@@ -0,0 +1,6 @@
+""" Credentials that won't get checked in to git. """
+
+import os
+
+WALKSCORE_API_KEY = os.environ.get('WALKSCORE_API_KEY', '')
+GEOCODE_API_KEY = os.environ.get('GEOCODE_API_KEY', '')
